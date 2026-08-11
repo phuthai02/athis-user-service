f3424782dcf9a1a3b010e9179d77c9734121cdb8
Doan Phu Thai <doanphuthai1152002@gmail.com>
2026-08-11 08:22:35 +0700

    init commit

---
+++ src/main/java/com/athis/userservice/AthisUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.athis.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthisUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthisUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/athis/userservice/AthisUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athis.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthisUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
